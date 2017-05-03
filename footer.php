b816309f37f4c91a64ec3840a41b10b978f90f76
Abbade <gabbade@gmail.com>
2017-05-03 16:34:01 -0300

    limpando

--- footer.php
+++
@@ -1,62 +0,0 @@
-<?php
-footer();
-global $data, $sitepress;
-?>
-<footer>
-		
-<div id="footer">
-	<div class="totop"><div class="gototop"><div class="arrowgototop"></div></div></div>
-	<div class="fshadow"></div>
-	
-	<div id="footerinside">
-	
-
-
-		<div class="footer_widget">
-			
-				<div class="footer_widget1">
-				<?php dynamic_sidebar( 'footer1' ); ?>
-				<?php if($data['showsocialfooter']){ ?>
-				<div class="socialfooter">
-				<h3><?php _e('SOCIALIZE WITH US','wp-musica'); ?></h3>
-				<div class="socialcategory"><?php socialLinkTop() ?></div>
-				</div>	
-				<?php } ?>				
-				</div>	
-				
-				<div class="footer_widget2">	
-				<?php dynamic_sidebar( 'footer2' ); ?>
-				</div>	
-				
-				<div class="footer_widget3">	
-				<?php dynamic_sidebar( 'footer3' ); ?>
-				</div>
-				
-				<div class="footer_widget4 last">	
-				<?php dynamic_sidebar( 'footer4' ); ?>
-				</div>
-				
-		</div>
-
-
-
-	</div>
-	
-	
-	
-		<div id="footerbwrap">
-			<div id="footerb">
-			<div class="footernav">
-					<?php if ( has_nav_menu( 'footer-menu' ) ) { wp_nav_menu( array( 'menu_class' => 'footernav','theme_location' => 'footer-menu' ) );} ?>
-			</div>
-			<div class="copyright">							<?php if (!function_exists('icl_object_id') or (ICL_LANGUAGE_CODE == $sitepress->get_default_language()) ) { echo stripText($data['copyright']); } else {  _e('&copy; 2011 All rights reserved. ','wp-musica'); } ?>
-
-			</div>
-		</div>
-	</div>
-</div>
-	
-
-</footer>	<?php wp_footer();  ?><script type="text/javascript" charset="utf-8"> jQuery(document).ready(function(){    jQuery("a[rel^='prettyPhoto']").prettyPhoto({theme:'light_rounded',show_title: false,deeplinking:false});  });</script>
-</body>
-</html>
--- searchform.php
+++
@@ -1,6 +0,0 @@
-	<form method="get" id="sidebarsearch" action="<?php echo get_bloginfo( 'home');  ?>" >
-		<div>
-			<input type="text" value="" name="s" id="keyword" />
-			<input type="submit" name="submit" id="searchsubmit" value="" /> 
-		</div>
-	</form>
